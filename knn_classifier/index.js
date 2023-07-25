d221634b0c1dac02c669cdbbbe61508b2a48d511
Vuksan <vuk.simunovic@gmail.com>
2023-07-25 15:59:05 +0200

    Simplify an algorithm for generating circuit inputs

--- knn_classifier/index.js
+++ knn_classifier/index.js
@@ -13,50 +13,53 @@ const data = [
     [0, 1, 9]
 ];
 
+const originalData = [...data];
+
 for (let i = 0; i < data.length; i += 1) {
     data[i] = [i + 1, ...data[i]];
 }
 
-const originalData = [...data];
-
-for (let i = 0; i < data.length; i += 1) {
+for (let i = 0; i < originalData.length; i += 1) {
     tree.insert(
         poseidon([
             i + 1,
-            ...data[i].slice(1)
+            ...originalData[i]
         ])
     );
 }
 
 function distance(obj, x) {
     d = 0;
-    for (let i = 1; i < obj.length; i += 1) {
-        d += Math.abs(obj[i] - x[i - 1]);
+    for (let i = 0; i < obj.length; i += 1) {
+        d += Math.abs(obj[i] - x[i]);
     }
     return d;
 }
 
 const x = [1, 1, 1];
 
-const checksum = originalData.reduce((acc, curr) => {
+const checksum = data.reduce((acc, curr) => {
     return (acc + curr[0]) % 32767; // we need modul since circom works under modul, so we don't overflow
 }, 0);
 console.log('Checksum: ', checksum);
 
-const checkprod = originalData.reduce((acc, curr) => {
+const checkprod = data.reduce((acc, curr) => {
     return (acc * curr[0]) % 32767; // we need modul since circom works under modul, so we don't overflow
 }, 1);
 console.log('Checkprod: ', checkprod);
 
-console.log('Distance: ', data.map((row) => distance(row, x)));
+console.log('Distance: ', originalData.map((row) => distance(row, x)));
 
-const sorted = data.sort((a, b) => distance(a, x) - distance(b, x));
+const sorted = data.sort((a, b) => {
+    // Lose the index to calculate distance
+    return distance(a.slice(1), x) - distance(b.slice(1), x);
+});
 console.log('Distance sorted: ', sorted);
 const sortedIndexes = sorted.map(row => row[0]);
 console.log('Sorted indexes: ', sorted.map(row => row[0]));
 
-console.log('Num rows: ', data[0].slice(1).length);
-console.log('Num columns: ', data[0].length);
+console.log('Num neighbours: ', originalData.length);
+console.log('Num coordinates: ', originalData[0].length);
 console.log('Commitment: ', tree.root);
 
 const proofPaths = [];
